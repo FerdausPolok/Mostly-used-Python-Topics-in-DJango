92d1c6661b48c97f226d8a4ef053f7d83c3b47f2
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-06-25 08:42:17 +0600

    list methods

---
+++ Lists/list_methods.py
@@ -0,0 +1,27 @@
+myList = ["Bhutan", "Nepal"]
+
+#append
+myList.append("Banglades")
+myList.append(2)
+#myList.append(["Coun1", "Coun2"]) #single element hishebe add hobe my list a
+
+#extend
+myList.extend(["Coun1", "Coun2"]) #single single add korte extend use korte hobe
+
+#insert method
+myList.insert(3, "Germany")
+
+#remove
+myList.remove("Coun1") #multi element thakle 1st ta remove korbe
+
+#pop - removes lit's last member or index member
+
+myList.pop()
+myList.pop(1)
+
+#clear
+myList.clear()
+
+
+print(myList)
+
