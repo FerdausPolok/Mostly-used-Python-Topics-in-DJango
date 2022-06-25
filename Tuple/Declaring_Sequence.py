6868ba80d6383e1ffb1335f71fe2c4d8ca94a302
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-06-25 09:02:07 +0600

    tple seq

---
+++ Tuple/Declaring_Sequence.py
@@ -0,0 +1,19 @@
+tp = 1,2,3
+tp2= 1
+print(type(tp))
+print(type(tp2))
+
+#have to use coma or first bracket
+
+tp = 1,2,3
+tp2= 1,
+print(type(tp))
+print(type(tp2))
+
+tp = (1,2,3)
+tp2= (1,)
+print(type(tp))
+print(type(tp2))
+
+#using homogenous/ same type of data => List
+#using heterogeneous / different type of data => tuple
