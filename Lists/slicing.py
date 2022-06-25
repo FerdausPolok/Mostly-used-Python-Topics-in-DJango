4c4ab9f4aef38d928b52fae2b463abab1e64fab7
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-06-25 07:34:36 +0600

    Update slicing.py

--- Lists/slicing.py
+++ Lists/slicing.py
@@ -1,6 +1,6 @@
 #sliping a lis - means cutting lists
 
-myList = ["Bhutan", "Nepal", "Bangladesh", "France", "Austria", "Denmark"]
+myList = ["Bhutan", "Nepal", "Bangladesh", "France", "Austria", "Denmark", "Brazil",  "Argentina", "UK", "Norway"]
 
 #getting the complete list
 newList = myList[:]
@@ -10,4 +10,22 @@ newList = myList[2:] #starts from- joto dibo toto theke
 newList = myList[:3] #upto - 5 er ag porjonto
 newList = myList[1:3]
 
+
+
+#stepping ::
+newList = myList[::] #shows full list
+newList = myList[::1] #shows full list
+newList = myList[::2] #Skips 1 - every 2nd member
+newList = myList[::3] #Skips 1 - every 2nd and 3rd member
+
+newList = myList[0:5:2]  #starting: ending+1 : skipping step -1
+newList = myList[-5:-1:2]
 print(newList)
+
+
+#slicing a string with same technique
+
+str = "FerdausZamanPolok"
+newStr = str[-5:-1:2]
+
+print(newStr)
