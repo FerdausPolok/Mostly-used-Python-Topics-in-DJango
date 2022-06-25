fd6353748f0dc7dbc81ee33dd158760439a05c96
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-06-25 10:35:43 +0600

    range

---
+++ Range/range_basic.py
@@ -0,0 +1,15 @@
+
+#range(start,stop-1,step)
+#range(10) -> range(0,10) -> 0-9
+
+a=list(range(10))
+print(a)
+
+a=list(range(5,11))
+print(a)
+
+a=list(range(2,101,2))
+print(a)
+
+a=list(range(10,-1,-1))
+print(a)
