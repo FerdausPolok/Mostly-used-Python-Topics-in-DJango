af8527158ffa510c39fbf84da6c09e5f7a258faa
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-06-25 10:14:31 +0600

    Create Set_operations.py

---
+++ Sets/Set_operations.py
@@ -0,0 +1,14 @@
+set1 = set("FERDAUS")
+print(set1)
+
+set2= set("ZAMAN")
+print(set2)
+
+#intersection
+print(set1 & set2) #dui set er common
+
+#union
+print(set1 | set2) #dui set er shobai at least 1 bar
+
+#difference
+print(set1 - set2) #set 1 theke set 2 er gula bad hoye print hobe
