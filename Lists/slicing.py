3c449499e65ce14283ba035c703738b099daa93c
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-06-25 07:22:25 +0600

    slicing part 1

---
+++ Lists/slicing.py
@@ -0,0 +1,13 @@
+#sliping a lis - means cutting lists
+
+myList = ["Bhutan", "Nepal", "Bangladesh", "France", "Austria", "Denmark"]
+
+#getting the complete list
+newList = myList[:]
+
+#: left is starting point and : right is ending point
+newList = myList[2:] #starts from- joto dibo toto theke
+newList = myList[:3] #upto - 5 er ag porjonto
+newList = myList[1:3]
+
+print(newList)
