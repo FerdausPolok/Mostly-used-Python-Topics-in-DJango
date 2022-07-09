b61559f2abca31170f9b5aa6e0313320671ff3dd
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-07-09 08:17:55 +0600

    super() method in py oop

--- OOP/super_method.py
+++ OOP/super_method.py
@@ -9,12 +9,15 @@ class A: #paren class
 class B(A): #child class
 
     #a er property b te ante chai
-    def __init__(self):
-        pass
+    def __init__(self,name, job):
+        #A.__init__(self, name)
+        super().__init__(name) #super use korle parent class er nam na janleo hobe
+        self.job=job
+
 
     def hello(self):
-        print("Hello from class b") #overriding
+        print(f"{self.name} is a {self.job}") #overriding
 
 
-obj = B()
+obj = B("Polok","Student")
 obj.hello()
