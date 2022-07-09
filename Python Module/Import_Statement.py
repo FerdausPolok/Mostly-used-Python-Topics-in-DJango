647e2978cbc4d6801b005ca563946976ef983eb4
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-07-09 10:02:59 +0600

    Import satement

---
+++ Python Module/Import_Statement.py
@@ -0,0 +1,13 @@
+#module means built in function or library that helps us to carry code
+
+import math
+
+print(math.ceil(5.2)) #upper value dibe
+print(math.floor(5.8)) #lower value dibe
+
+import oop
+
+print(oop.add(5,10))
+print(oop.pi)
+
+obj = oop.A()
